d8cd0b21967797fa8a3cb6a2df1a277d2d17cf07
MoisesL27 <leonormoises27@outlook.com>
2021-03-13 11:32:17 -0600

    add styles to customize default bootstrap carousel styles

--- src/app/features/feature-property/components/property-overview/property-overview.component.ts
+++ src/app/features/feature-property/components/property-overview/property-overview.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-property-overview',
   templateUrl: './property-overview.component.html',
   styleUrls: ['./property-overview.component.scss'],
+  providers: [NgbCarouselConfig],
 })
 export class PropertyOverviewComponent {
   navbarItems = ['Fotos', 'Menu'];
   navbarSelectedItem = 0;
 
   images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+
+  constructor(config: NgbCarouselConfig) {
+    // customize default values of carousels used by this component tree
+    config.showNavigationArrows = true;
+    config.showNavigationIndicators = false;
+  }
 
   itemChange(index: number): void {
     this.navbarSelectedItem = index;
