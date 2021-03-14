2b96039fdeabfdb7f16aac80487b92f53ce49f72
MoisesL27 <leonormoises27@outlook.com>
2021-03-13 21:10:28 -0600

    add styles to show more and show less in description component

--- src/app/features/feature-property/components/property-description/property-description.component.ts
+++ src/app/features/feature-property/components/property-description/property-description.component.ts
@@ -1,6 +1,7 @@
-import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { Property } from '@shared/models';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-property-description',
@@ -9,5 +10,18 @@ import { Observable } from 'rxjs';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PropertyDescriptionComponent {
+  private collapseHeigth = new BehaviorSubject('0');
+
   @Input() property$: Observable<Property>;
+
+  collapseHeigth$ = this.collapseHeigth.asObservable();
+  isOpen$ = this.collapseHeigth.pipe(map((value) => value !== '0'));
+
+  toggleCollapse(element: HTMLDivElement): void {
+    if (this.collapseHeigth.value !== '0') {
+      this.collapseHeigth.next('0');
+      return;
+    }
+    this.collapseHeigth.next(element.scrollHeight + 'px');
+  }
 }
